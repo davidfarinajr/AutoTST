c6abce9830631033ced0bc0a1be210b82dfd6011
davidfarinajr <farina.d@husky.neu.edu>
2019-06-27 16:02:19 -0400

    added unit test for orca calc

--- autotst/calculator/orcaTest.py
+++ autotst/calculator/orcaTest.py
@@ -11,16 +11,25 @@ class TestOrca(unittest.TestCase):
 
     def setUp(self):
         conf = Conformer(smiles='C')
-        self.orca = Orca(conformer=conf)
+        self.orca = Orca(conformer=conf, directory=os.path.expandvars(
+            "$AUTOTST/autotst/calculator/fod"))
+
+    def test_load_conformer_attributes(self):
+        charge = 0
+        mult = 1
+        label = 'CC(C)C'
+        base = 'CC{C}C'
+        self.orca.conformer = Conformer(smiles='CC(C)C')
+        self.orca.load_conformer_attributes()
+        self.assertTrue([charge,mult,label,base] == 
+                        [self.orca.charge,self.orca.mult,self.orca.label,self.orca.base])
 
     def test_write_fod_input(self):
-        path = os.path.expandvars(
-            "$AUTOTST/autotst/calculator/fod")
-        if os.path.exists(path):
-            shutil.rmtree(path)
-        os.makedirs(path)
-        self.orca.write_fod_input(path=path)
-        self.assertTrue(os.path.exists(os.path.join(path,'C_fod.inp')))
+        if os.path.exists(self.orca.directory):
+            shutil.rmtree(self.orca.directory)
+        os.makedirs(self.orca.directory)
+        self.orca.write_fod_input()
+        self.assertTrue(os.path.exists(os.path.join(self.orca.directory,'C_fod.inp')))
 
     def test_check_NormalTermination(self):
         path = os.path.expandvars(
